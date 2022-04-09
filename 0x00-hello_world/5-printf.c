675f05c746739631a1f948ccb70b0802c029da9b
Okwara Uche <souljaboy.ou@gmail.com>
2022-04-09 07:31:18 -0700

    my fifth c task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *My hello, printf task
- *Return: return 1 after program execution
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
