4498e83689233dc8d7bd5b469e99809a707d22ca
Okwara Uche <souljaboy.ou@gmail.com>
2022-04-09 07:38:30 -0700

    my fifth c task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ *Description: main - Prints out a string to stdout.
+ *Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
