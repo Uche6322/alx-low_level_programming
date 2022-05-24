ce995cac4500958a48fff1896cba4715cde0443f
Okwara Uche <souljaboy.ou@gmail.com>
2022-05-24 10:59:52 -0700

    my commit message

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,15 +10,16 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ * Auth: Martins Akhivbareme
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
